e668f076b7be58b8e138525293160dbeb7852bcb
Александра Гаврина <alexgav2009@mail.ru>
2024-01-26 20:40:13 +0300

    Добавьте файлы проекта.

---
+++ task3_2/BaseArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_2
+{
+    public abstract class BaseArray
+    {
+        //
+        // Сводка:
+        //     Заполнение массива целыми числами
+        public abstract void GetIntValues();
+
+
+        //
+        // Сводка:
+        //     Заполнение массива случайными числами
+        public abstract void GetRandomValues();
+
+        //
+        // Сводка:
+        //     Выводит на экран массив
+        public abstract void PrintArray();
+
+        //
+        // Сводка:
+        //     Считает среднее значение массива
+        //
+        // Возврат:
+        //     Дробное число, сумма, поделенная на количество элементов
+        public abstract double AverageValue();
+    }
+
+}
---
+++ task3_2/MultiplyDArray.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_2
+{
+    sealed class MultiplyDArray : BaseArray
+    {
+        private int[,] _array;
+        private int _row;
+        private int _col;
+
+        public MultiplyDArray(int row, int collumn, bool isUser = false)
+        {
+            _array = new int[row, collumn];
+            _row = row;
+            _col = collumn;
+            if (!isUser)
+            {
+                GetRandomValues();
+            }
+            else
+            {
+                GetIntValues();
+            }
+        }
+
+        public override double AverageValue()
+        {
+            int count = 0;
+            double sum = 0;
+            for (int i = 0; i < _row; i++)
+            {
+                for (int j = 0; j < _col; j++)
+                {
+                    sum += _array[i, j];
+                    count++;
+                }
+            }
+
+            return sum / count;
+        }
+
+
+        public override void GetIntValues()
+        {
+            for (int i = 0; i < _row; i++)
+            {
+                for (int j = 0; j < _col; j++)
+                {
+                    Console.WriteLine($"элемент № {j}");
+
+                    _array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public override void GetRandomValues()
+        {
+
+            Random random = new Random();
+            for (int i = 0; i < _row; i++)
+            {
+                for (int j = 0; j < _col; j++)
+                {
+                    _array[i, j] = random.Next(0, 255);
+                }
+            }
+        }
+
+        public override void PrintArray()
+        {
+            int rows = _array.GetLength(0);
+            int columns = _array.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(_array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+}
