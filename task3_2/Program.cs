0495506af26b4b74f527abd7be6fd42af1ddb4db
Александра Гаврина <alexgav2009@mail.ru>
2024-01-26 20:49:20 +0300

    Revert "Revert "Добавьте файлы проекта.""
    This reverts commit fab63b442a330cbf2e13f06485d7042b6f9ade93.

---
+++ task3_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            OneDArray one = new OneDArray(4);
+            BaseArray two = new OneDArray(3);
+
+            MultiplyDArray three = new MultiplyDArray(4, 4);
+            BaseArray four = new MultiplyDArray(4, 5);
+
+            SteppedArray five = new SteppedArray(4);
+            BaseArray six = new SteppedArray(5);
+
+            BaseArray[] baseArrays = new BaseArray[6]
+            {
+                one, two, three, four, five, six
+            };
+
+            foreach(BaseArray item in baseArrays)
+            {
+                Console.WriteLine($"Среднее значение {item.GetType()} массива {item.AverageValue()}");
+                Console.WriteLine($"Элементы массива {item.GetType()}");
+                item.PrintArray();
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+    
+}
